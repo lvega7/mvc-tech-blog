c170403fccd198ee026b37f19c5c4bb507cd44f9
Lissette Vega <lissettevega@Lissettes-MacBook-Pro.local>
2021-11-27 19:24:40 -0500

    Initialize files/ add modesl, api, handlebars

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const routes = require('./controllers/');
+const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
